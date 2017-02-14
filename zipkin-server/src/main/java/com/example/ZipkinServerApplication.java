1554fa896d054a6739c170babe9a7c97ae56c349
Remigiusz Dudek <remigiusz_dudek@tlen.pl>
2017-02-14 06:22:44 +0100

    enable zipkin server

--- zipkin-server/src/main/java/com/example/ZipkinServerApplication.java
+++ zipkin-server/src/main/java/com/example/ZipkinServerApplication.java
@@ -2,8 +2,10 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import zipkin.server.EnableZipkinServer;
 
 @SpringBootApplication
+@EnableZipkinServer
 public class ZipkinServerApplication {
 
 	public static void main(String[] args) {
